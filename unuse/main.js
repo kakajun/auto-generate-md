fdeeaa22d5b158e1979c9c74f0c1b41ace26ae50
马军 <majun@xiwicloud.com>
2022-04-06 13:58:47 +0800

    fix: 1

--- unuse/main.js
+++ unuse/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // import '../lib/style.css'
 import SketchRule from "./components/test2/HelloWorld.vue";
+import moduleName from '@/api/aa';
 const app = createApp(App)
 // app.use(SketchRule);
 import './mixins.js'
