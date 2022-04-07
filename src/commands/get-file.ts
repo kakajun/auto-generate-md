3a0d0af1c1dd7c2c4af71ee4b4ba1299d32de663
马军 <majun@xiwicloud.com>
2022-04-07 09:39:12 +0800

    fix: 1

--- src/commands/get-file.ts
+++ src/commands/get-file.ts
@@ -80,6 +80,7 @@ export function getFileNodes(
 ): Array<ItemType> {
   //File filtering -- full name with suffix required  文件过滤--需要全称带后缀
   let ignore = [
+    'jest.config.js',
     'router',
     'img',
     'styles',
