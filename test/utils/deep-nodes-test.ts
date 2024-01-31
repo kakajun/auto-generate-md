abfbd91a17e039a7974705c984b38c5b95629cb5
马军 <majun@xiwicloud.com>
2024-01-31 08:57:04 +0800

    fix: 1

--- test/utils/deep-nodes-test.ts
+++ test/utils/deep-nodes-test.ts
@@ -1,3 +1,4 @@
+const rootPath = process.cwd().replace(/\\/g, '/')
 const nodeComponents = [
   {
     name: 'test',
@@ -20,18 +21,18 @@ const nodeComponents = [
             isDir: false,
             level: 2,
             note: '//2工程',
-            imports: ['D:/gitwork/auto-generate-md/api/user.js'],
+            imports: [rootPath + '/api/user.js'],
             belongTo: [],
             size: 1791,
             rowSize: 109,
             suffix: '.vue',
-            fullPath: 'D:/gitwork/auto-generate-md/unuse/components/test/deep/user.vue'
+            fullPath: rootPath + '/unuse/components/test/deep/user.vue'
           }
         ],
-        fullPath: 'D:/gitwork/auto-generate-md/unuse/components/test/deep'
+        fullPath: rootPath + '/unuse/components/test/deep'
       }
     ],
-    fullPath: 'D:/gitwork/auto-generate-md/unuse/components/test'
+    fullPath: rootPath + '/unuse/components/test'
   },
   {
     name: 'test2',
@@ -46,27 +47,27 @@ const nodeComponents = [
         isDir: false,
         level: 1,
         note: '//2工程',
-        imports: ['D:/gitwork/auto-generate-md/unuse/components/test/deep/user.vue'],
+        imports: [rootPath + '/unuse/components/test/deep/user.vue'],
         belongTo: [],
         size: 411,
         rowSize: 31,
         suffix: '.vue',
-        fullPath: 'D:/gitwork/auto-generate-md/unuse/components/test2/HelloWorld.vue'
+        fullPath: rootPath + '/unuse/components/test2/HelloWorld.vue'
       }
     ],
-    fullPath: 'D:/gitwork/auto-generate-md/unuse/components/test2'
+    fullPath: rootPath + '/unuse/components/test2'
   },
   {
     name: 'user-rulerts.vue',
     isDir: false,
     level: 0,
     note: '',
-    imports: ['D:/gitwork/auto-generate-md/unuse/components/test/deep/user.vue'],
+    imports: [rootPath + '/unuse/components/test/deep/user.vue'],
     belongTo: [],
     size: 2503,
     rowSize: 105,
     suffix: '.vue',
-    fullPath: 'D:/gitwork/auto-generate-md/unuse/components/user-rulerts.vue'
+    fullPath: rootPath + '/unuse/components/user-rulerts.vue'
   }
 ]
 
