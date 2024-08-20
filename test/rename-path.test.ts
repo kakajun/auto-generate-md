592df4e8a2875319dc1e7152ea338d7d075783c2
马军 <253495832@qq.com>
2024-08-20 08:36:19 +0800

    🐞 fix:test

--- test/rename-path.test.ts
+++ test/rename-path.test.ts
@@ -6,20 +6,20 @@ jest.mock('fs')
 describe('replaceName function tests', () => {
   beforeEach(async () => {
     vol.reset()
-    await fs.mkdirp('/pathA/to/ExampleDirectory/')
-    fs.writeFileSync('/pathA/to/example-file.txt', 'file contents')
-    fs.writeFileSync('/pathA/to/FoolFile.txt', 'fool contents')
+    await fs.mkdirp('./pathA/to')
+    fs.writeFileSync('./pathA/to/example-file.txt', 'file contents')
+    fs.writeFileSync('./pathA/to/FoolFile.txt', 'fool contents')
   })
 
   test('should rename a file to camelCase', async () => {
-    const fullPath = '/pathA/to/example-file.txt'
+    const fullPath = './pathA/to/example-file.txt'
     const result = await replaceName(fullPath, true)
     expect(result.newName).toBe('ExampleFile.txt')
     expect(result.filename).toBe('example-file.txt')
   })
 
   test('should rename a file to kebab-case', async () => {
-    const fullPath = '/pathA/to/FoolFile.txt'
+    const fullPath = './pathA/to/FoolFile.txt'
     const result = await replaceName(fullPath)
     expect(result.newName).toBe('fool-file.txt')
     expect(result.filename).toBe('FoolFile.txt')
